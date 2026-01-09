3ec9be89fdd5f886840356c08d9d6c30bb8f4855
anhkhoinp56 <anhkhoi.np56@gmail.com>
2026-01-09 12:49:26 +0700

    2026-01-09_1249 PM

---
+++ addBreakEvery2Lines.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+const inputFile = "lyrics\\Cam_20_Test_1_Part_2.txt";
+const outputFile = "lyrics\\Cam_20_Test_1_Part_2.txt";
+
+const data = fs.readFileSync(inputFile, "utf8");
+
+// Split by line (handles Windows & Unix)
+const lines = data.split(/\r?\n/);
+
+let result = [];
+
+for (let i = 0; i < lines.length; i++) {
+  result.push(lines[i]);
+
+  // Add blank line after every 2 lines (except at end)
+  if ((i + 1) % 2 === 0 && i !== lines.length - 1) {
+    result.push("");
+  }
+}
+result.push("");
+fs.writeFileSync(outputFile, result.join("\n"), "utf8");
+
+console.log("Done!");
